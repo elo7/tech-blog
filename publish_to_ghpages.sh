5f77f7c3586faa30ca95ddd843cefad461380607
Fernanda Moraes Bernardo <login+FernandaBernardo@elo7.com>
2019-03-25 08:27:06 -0300

    tenta deployar para branch de test

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -16,7 +16,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B test-pages public origin/test-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -27,3 +27,4 @@ npm run build
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git push origin test-pages
