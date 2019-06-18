ce03a34ab8052df8292632b2f8c410e142958364
Fernanda Bernardo <fernandabernardo94@gmail.com>
2019-06-18 13:37:30 -0300

    WIP

--- static/js/search.js
+++ static/js/search.js
@@ -1,21 +1,25 @@
 define(['doc', 'ajax'], function($, ajax) {
+    var urlQueryValue = window.location.search,
+        searchParams = new URLSearchParams(urlQueryValue);
     var url = 'http://blog-api-dev.kube.aws.elo7.io/api/v1/search';
     var $postContainer = $('.search');
     ajax.get(url, {
-        q: 'teste',
+        q: searchParams.get('query'),
         pg: 1,
         rows: 5
     }, {
         success: function(response, xhr) {
             var posts = response.docs;
+
             posts.forEach(p => {
-                var postTitle = p.title,
+                var postTitle = p.title.replace("Elo7 Tech - ", ""),
                     postUrl = p.url;
                 var article = document.createElement('article'),
-                    pTitle = document.createElement('p');
+                    a = document.createElement('a');
                 
-                $(pTitle).text(postTitle);
-                article.append(pTitle);
+                $(a).addClass('link-search').html(postTitle);
+                a.href = postUrl;
+                article.append(a);
                 $postContainer.append(article);
             });
         }
